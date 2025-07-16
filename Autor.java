72f91d8ffe6e8649420613e4d59f316220acd21e
Emanuele Fernanda Ferraz de Araújo <emanuelearaujo@ufpr.br>
2025-07-16 15:06:05 -0300

    Create Autor.java

---
+++ Autor.java
@@ -0,0 +1,14 @@
+class Autor {
+    String nome;
+    String nacionalidade;
+
+    // Construtores
+    public Autor() {}
+    public Autor(String nome) {
+        this.nome = nome;
+    }
+    public Autor(String nome, String nacionalidade) {
+        this.nome = nome;
+        this.nacionalidade = nacionalidade;
+    }
+}
