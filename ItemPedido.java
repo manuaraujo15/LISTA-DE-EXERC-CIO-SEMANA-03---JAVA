fb45874e8d930e4bcbdc08eb950f8de41ea3d4bb
Emanuele Fernanda Ferraz de Araújo <emanuelearaujo@ufpr.br>
2025-07-16 15:14:52 -0300

    Create ItemPedido.java

---
+++ ItemPedido.java
@@ -0,0 +1,10 @@
+public class ItemPedido {
+    String nomeItem;
+    double preco;
+
+    public ItemPedido() {}
+    public ItemPedido(String nomeItem, double preco) {
+        this.nomeItem = nomeItem;
+        this.preco = preco;
+    }
+}
