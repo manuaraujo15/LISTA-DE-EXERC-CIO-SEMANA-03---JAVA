d4d1748892ed5005bd6cfc6b6fcbd050f8f2ba5a
Emanuele Fernanda Ferraz de Araújo <emanuelearaujo@ufpr.br>
2025-07-16 15:09:16 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -2,16 +2,27 @@ class Livro {
     String titulo;
     int ano;
     Autor autor;
+    int edicao;
 
-    // Construtores
     public Livro() {}
+
     public Livro(String titulo, int ano) {
         this.titulo = titulo;
         this.ano = ano;
     }
-    public Livro(String titulo, int ano, Autor autor) {
+
+    public Livro(String titulo, int ano, Autor autor, int edicao) {
         this.titulo = titulo;
         this.ano = ano;
         this.autor = autor;
+        this.edicao = edicao;
+    }
+
+    public void exibir() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Ano: " + ano);
+        System.out.println("Autor: " + autor.nome);
+        System.out.println("Edição: " + edicao);
+        System.out.println();
     }
 }
